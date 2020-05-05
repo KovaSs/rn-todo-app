914eeadc7e169ee0f209f93f081a9d724039fd45
Isaev Denis <sr.relikt@gmail.com>
2020-05-05 19:51:32 +0300

    feature | add todo item component

--- App.tsx
+++ App.tsx
@@ -4,15 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar'
 import { AddTodo } from './src/AddTodo'
 import { useApp } from './src/hooks'
+import { TodoItem } from './src/TodoItem';
 
 export default function App() {
-  const {addTodo} = useApp();
+  const { todos, addTodo } = useApp();
+
+  const renderTodoList = todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
 
   return (
     <View>
       <Navbar title="Todo App" />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
+        <View>
+          {renderTodoList}
+        </View>
       </View>
     </View>
   );
