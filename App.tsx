dce664cedf91d5d0067ee61dbc2ad92d028dc1ca
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2022-01-01 03:24:00 +0300

    lesson 13 | manipalate styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Navbar } from './src/Navbar';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Navbar />
+      <Text>Hello react native!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/Navbar.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export const Navbar = () => {
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.text}>TODO app</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  navbar: {},
+  text: {}
+});
